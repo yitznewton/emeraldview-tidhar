594d95c645cf3aa38539467a52ba829f09fedf15
yitznewton <yitznewton@hotmail.com>
2011-12-28 13:52:07 -0500

    customize error page for Tidhar

--- public/views/kohana_error_page.php
+++ public/views/kohana_error_page.php
@@ -1,103 +1,157 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
+  "http://www.w3.org/TR/html4/loose.dtd">
 
 <html>
 
-<head>
-<title>Error | EmeraldView</title>
+  <head>
+    <title>Encyclopedia of the Founders and Builders of Israel | אנציקלופדיה לחלוצי הישוב ובוניו</title>
 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="author" content="David Tidhar">
+    <meta name="author" lang="he" dir="rtl" content="דוד תדהר">
+    <meta name="author" content="Touro College Libraries">
 
-<link rel="stylesheet" type="text/css"
-href="<?php echo url::base() ?>views/default/css/reset.css">
+    <meta name="description" content="A biographical dictionary or encyclopedia of 19th and 20th century Israelis">
+    <meta name="keywords" content="israel, biographies, jews, hebrew, history, jewish, genealogy">
 
-<link rel="stylesheet" type="text/css"
-href="<?php echo url::base() ?>views/default/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/views/tidhar/css/aesthete.css">
+    <link rel="stylesheet" type="text/css" href="/views/tidhar/css/style.css">
 
-<link rel="stylesheet" type="text/css" media="print"
-href="<?php echo url::base() ?>views/default/css/style-print.css">
+  </head>
 
-<script type="text/javascript"
-src="<?php echo url::base() ?>libraries/jquery.js"></script>
+  <body dir="ltr">
 
-<script type="text/javascript"
-src="<?php echo url::base() ?>views/default/js/default.js"></script>
+    <div id="page">
+      <div id="wrapper">
 
-</head>
+        <div id="header">
+          <a href="/" class="headerimg">
+            <img src="/views/tidhar/images/banner.jpg" border="0" class="headerimg" alt="encyclopedia banner"/>
+          </a>
+          <div class="headertitle">
+            <a href="/">
+              <img src="/views/tidhar/images/banner_title.png"
+                   alt="Encyclopedia of the Founders and Builders of Israel" />
+            </a>
+          </div>
 
-<body>
+             
+         <div class="clear"></div>
 
-<div id="header">
-    <span id="header-logo">
-      <a href="<?php echo url::base() ?>">
-        <img src="<?php echo url::base() ?>images/emeraldview2.png"
-        alt="EmeraldView logo" />
-      </a>
-    </span>
-    <div id="header-emeraldview-title">
-      <div>EmeraldView</div>
-    </div>
-</div>
+        </div>
 
-  <div id="main-content">
-    <?php if ( IN_PRODUCTION ): ?>
-      <h1>Error</h1>
-      
-      <p>
-        We're sorry, our system encountered an error. Please try again later.
-      </p>
-    <?php else: ?>
-      <h1>Server error</h1>
 
-      <div id="framework_error" style="width:42em;margin:20px auto;">
-      <h3><?php echo html::specialchars($error) ?></h3>
-      <p><?php echo html::specialchars($description) ?></p>
-      <?php if ( ! empty($line) AND ! empty($file)): ?>
-      <p><?php echo Kohana::lang('core.error_file_line', $file, $line) ?></p>
-      <?php endif ?>
-      <p><code class="block"><?php echo $message ?></code></p>
-      <?php if ( ! empty($trace)): ?>
-      <h3><?php echo Kohana::lang('core.stack_trace') ?></h3>
-      <?php echo $trace ?>
-      <?php endif ?>
+        <div id="whitepage">
+          <div id="whitepagesidebg">
+            <div id="whitepagetopbg">
+              <div id="whitepagebottombg">
+                <div id="navdiv">
+
+                  <ul id="nav">
+                    <li class="page_item">
+                      <a href="/" title="Home">Home</a>
+                    </li>
+
+                    <li class="page_item">
+                      <a href="#">Browse</a>
+                      <ul>
+
+                        <li class="page_item">
+                          <a href="/tidhar/browse/entries" title="By entry">By entry</a>
+                        </li>
+                        <li class="page_item">
+                          <a href="/tidhar/browse/volume" title="By volume">By volume</a>
+                        </li>
+                      </ul>
+                    </li>
+
+                    <li class="page_item">
+                      <a href="/tidhar/contact">Contact</a>
+                    </li>
+
+                    <li class="page_item" id="touro-link">
+                      A project of <a href="http://www.tourolib.org/">Touro College Libraries</a>
+                    </li>
+
+                  </ul>
+                  <div class="clear"></div>
+                </div>
+
+
+                <!-- end header --><div id="leftcontent">
+
+                <h1>Error</h1>
+
+                <p>We're sorry, something is wrong with our systems. Please
+                try again in a few minutes, or contact us at
+                <a href="http://www.tourolib.org/tech-support">Tech Support</a>
+                or <a href="mailto:systems.library@touro.edu">systems.library@touro.edu</a></p>
+
+                </div>
+                <div id="sidebar">
+                  <div class="widget widget_categories">
+
+                    <div class="ornament random-entries">
+                      <h3>Random entries</h3>
+                      <ul dir="rtl" id="random-entries" class="rtl">
+<li><a href="http://www.tidhar.tourolib.org/tidhar/view/16/4963">
+אברהם יצחק נאמן</a>
+</li>
+<li><a href="http://www.tidhar.tourolib.org/tidhar/view/6/2612">
+יצחק יצחקי</a>
+</li>
+<li><a href="http://www.tidhar.tourolib.org/tidhar/view/9/3380">
+צבי אליעזר קרוי</a>
+
+</li>
+</ul>
+                    </div>
+                  </div>
+                  <div class="widget widget_categories">
+                    <div class="share-discovery">
+                      Did you find something remarkable?
+                      <a href="/tidhar/contact">Share your discovery</a>
+                      with us!
+
+                      <ul>
+
+<li><q>It is difficult to overstate the significance of this project.</q><cite>Haaretz</cite></li>
+<li><q>Tidhar's Encyclopedia online is like an 'oracle' to me.</q><cite>David Hacohen, Tel Aviv</cite></li>
+<li><q>I never saw a picture of my great great grandfather &ndash; and here he was.</q><cite>batyama</cite></li>
+</ul>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="clear"></div>
+                <div id="footer">
+                  <div id="footer-url">URL: http://www.tidhar.tourolib.org/tidhar</div>
+
+                  <div>Copyright &copy; 2011 by the Estate of David Tidhar and Touro College Libraries</div>
+
+                                  </div>
+
+              </div> <!-- "whitepagebottombg -->
+            </div>
+
+          </div>
+        </div>
       </div>
-    <?php endif; ?>
-  </div>
+    </div>
 
-<div id="footer">
-  <div id="footer-url">URL: <?php echo url::site( url::current( true ) ) ?></div>
+    
+    <script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20322458-2']);
+  _gaq.push(['_trackPageview']);
 
-  <div>Copyright &copy; <?php echo date('Y') ?>
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 
-  <?php
-  if (
-    EmeraldviewConfig::get('institution_name')
-    && EmeraldviewConfig::get('institution_url')
-  ) {
-    $inst_name = EmeraldviewConfig::get('institution_name');
-    $inst_url  = EmeraldviewConfig::get('institution_url');
-
-    echo 'by ';
-    echo html::anchor( $inst_url, $inst_name );
-  }
-  elseif (EmeraldviewConfig::get('institution_name')) {
-    echo 'by ' . EmeraldviewConfig::get('institution_name') ;
-  }
-  ?>
-
-  </div>
-
-  <div>Powered by
-    <a href="http://www.greenstone.org/">Greenstone</a> and
-    <a href="http://bitbucket.org/yitznewton/emeraldview">EmeraldView</a>
-  </div>
-
-  <?php if ( ! IN_PRODUCTION ): ?>
-  <div>
-    Rendered in {execution_time} seconds, using {memory_usage} of memory
-  </div>
-  <?php endif; ?>
-</div>
-
-</body>
+  </body>
 </html>
+
